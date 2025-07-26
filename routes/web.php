b77887d4a485bc31c937cff15f2fe34353436fc7
Amr Tamer Elharery <amrelharery@gmail.com>
2025-07-26 15:51:47 +0300

    Fix login route

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\DoctorController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/login', function () {
     return view('login');
 })->name('login');
 
